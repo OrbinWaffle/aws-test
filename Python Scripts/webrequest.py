db79efd15a38905e5363a16536473799f731c4e4
OrbinWaffle <79740022+OrbinWaffle@users.noreply.github.com>
2023-10-12 11:48:37 -0700

    api stuff

--- Python Scripts/webrequest.py
+++ Python Scripts/webrequest.py
@@ -3,26 +3,43 @@ import requests
 import json
 
 # api-endpoint
-URL = "https://cniyl5hf4nr7qy6m4bkhjclfii0umqoh.lambda-url.us-east-2.on.aws/"
+# URL = "https://cniyl5hf4nr7qy6m4bkhjclfii0umqoh.lambda-url.us-east-2.on.aws/"
+URL = "https://wu003u1jrj.execute-api.us-east-2.amazonaws.com/"
 
-fname = input("Please provide your first name: ")
-lname = input("Please provide your last name: ")
+def main():
+    user_input = input("What do you want to do: ")
+    if(user_input == "0"):
+      say_hi()
+    elif(user_input == "1"):
+       greet()
 
-print("Formatting json...")
-BODY = { 
-    "Data": [
-        {
-            "first_name":fname,
-            "last_name":lname
-        }
-    ]
-}
+def say_hi():
+    send_request(URL + "say_hi", "")
 
-print("Request formatted. Sending to Lambda...")
-r = requests.get(url = URL, json=BODY)
-print("Response recieved.")
-print(str(r) + "\n")
+def greet():
+    fname = input("Please provide your first name: ")
+    lname = input("Please provide your last name: ")
 
-data = r.json()
+    print("Formatting json...")
+    BODY = { 
+        "Data": [
+            {
+                "first_name":fname,
+                "last_name":lname
+            }
+        ]
+    }
+    send_request(URL + "greeting", BODY)
 
-print(data)
+def send_request(newURL, newBody):
+    print("Request formatted. Sending to Lambda...")
+    r = requests.get(url = newURL, json=newBody)
+    print("Response recieved.")
+    print(str(r) + "\n")
+
+    data = r.json()
+
+    print(data)
+
+if(__name__ == "__main__"):
+   main()
